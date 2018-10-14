97636f775ef8df0df2ca6bff3c93da91baf6033b
Dan Upshaw <daniel.upshaw@720strategies.com>
2018-10-14 01:02:15 -0400

    Start on Bootstrap 4 theme

---
+++ htdocs/themes/bootstrap4/js_src/main.js
@@ -0,0 +1,6 @@
+// require ../lib/jquery/jquery.min.js
+// require ../lib/bootstrap/js/bootstrap.js
+// require ./jquery-ui-selectable-combined.min.js
+// require ./jquery.timers.js
+// require ./filereader.js
+// require ./stikked.js
